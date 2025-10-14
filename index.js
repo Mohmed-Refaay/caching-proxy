9684358c611d96c5ff2d5a1c1d47eef915f27b6e
Mohamed Amr Refaay <mohamedamrrefaay@gmail.com>
2025-10-14 18:10:09 +0300

    add basic caching

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
+import fetch from "node-fetch";
 import http from "node:http";
+import morgan from "morgan";
 
-const server = http.createServer((req, res) => {
-  console.log(req.headers);
-  console.log(req.method);
+const TARGET_HOST = "https://dummyjson.com";
 
-  console.log("new connection");
-  res.end("hello");
+const logger = morgan("dev");
+
+const cachedData = {};
+
+const server = http.createServer(async (req, res) => {
+  logger(req, res, async (err) => {
+    const path = req.url;
+    const method = req.method;
+
+    let data;
+    let contentType;
+    let statusCode;
+    if (method === "GET" && path in cachedData) {
+      console.log("Found!");
+      data = cachedData[path].data;
+      contentType = cachedData[path].contentType;
+      statusCode = cachedData[path].statusCode;
+    } else {
+      console.log("Not Found!");
+      const result = await fetch(`${TARGET_HOST}${path}`, {
+        method,
+      });
+
+      data = await result.arrayBuffer();
+      data = Buffer.from(data);
+      contentType =
+        result.headers.get("Content-Type") ?? "application/json";
+      statusCode = result.status;
+      if (result.ok) {
+        cachedData[path] = {
+          data,
+          contentType,
+          statusCode,
+        };
+      }
+    }
+
+    res.setHeader("Content-Type", contentType);
+    res.statusCode = statusCode;
+    res.end(data);
+  });
 });
 
 server.listen(8080, "127.0.0.1");
